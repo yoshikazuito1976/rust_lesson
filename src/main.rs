9e5ee5c1c95ce2e5c8a127db7bc6f898e0845336
Yoshikazu ITO <aisyu1976@gmail.com>
2025-01-04 11:08:25 +0900

    if分

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+
+
+
 fn main() {
-    println!("Hello, world!");
+    let x=30;
+
+    if x > 0 {
+        println!("OK");
+
+    }
+
+    if x > 0 && x< 10{
+        println!("0 < x and x<10");
+    }
+    if x > 0 || x < 10{
+        println!("0 < x or x < 10");
+        }
+    if x > 0 && x <= 10 {
+        println!("first condition");
+
+    } else if x > 11 && x <= 20{
+        println!("second condition");
+    } else{
+        println!("else");
+    }
 }
